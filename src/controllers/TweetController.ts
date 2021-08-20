cd321fbd5888507196ac662e09199b7686df916e
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-20 14:58:55 -0300

    created TweetController Query findTweets and Mutation create Tweets

---
+++ src/controllers/TweetController.ts
@@ -0,0 +1,24 @@
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+
+import Tweet from "../schemas/Tweet";
+import MongoTweet from '../database/schemas/Tweet';
+
+@Resolver(Tweet)
+class TweetController {
+    @Query( returns => [Tweet], { name: 'tweets' } )
+    async find() {
+        const tweets = await MongoTweet.find();
+        return tweets;
+    };
+
+    @Mutation( returns => Tweet, { name: 'createTweets' } )
+    async create(
+        @Arg( "author" ) author: string,
+        @Arg( "description" ) description: string,  
+    ) {
+        const tweet = await MongoTweet.create({ author, description, likes: 0 });
+        return tweet;
+    };
+};
+
+export default TweetController;
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,10 @@
 import { buildSchema } from "type-graphql";
 
+import TweetController from "../controllers/TweetController";
 import Tweet from './Tweet';
 
 const schema = async () => await buildSchema({
-    resolvers: [Tweet],
+    resolvers: [Tweet, TweetController],
 });
 
 export default schema;
